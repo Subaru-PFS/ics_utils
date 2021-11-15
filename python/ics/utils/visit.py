4b958c8d431f0f7029b5f6ef710f25e02572ad54
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-11-15 12:31:50 -1000

    add Visit0 class, getVisit take now a consumer argument

--- python/ics/utils/visit.py
+++ python/ics/utils/visit.py
@@ -23,19 +23,44 @@ class VisitManager(object):
     def __init__(self, actor):
         self.actor = actor
         self.activeVisit = dict()
+        self.visit0 = None
 
-    def newVisit(self, name=None):
+    @property
+    def visit0isValid(self):
         """ Allocate a new visit. """
-        # if self.activeVisit is not None:
+        return self.visit0 is not None
+
+    def declareNewField(self):
+        """ Allocate a new visit. """
+        visitId = self._fetchVisitFromGen2()
+        self.visit0 = VisitO(visitId)
+        return self.visit0
+
+    def resetVisit0(self):
+        """ Allocate a new visit. """
+        self.visit0 = None
+
+    def getVisit(self, consumer, name=None):
+        """ Allocate a new visit. """
+        if self.visit0isValid and self.visit0.getVisit(consumer):
+            return self.visit0
+
+        return self.newVisit(consumer, name=name)
+
+    def newVisit(self, consumer, name=None):
+        """ Allocate a new visit. """
+        # if self.perConsumer(consumer) is not None:
         #     raise VisitActiveError()
 
         visit = self._fetchVisitFromGen2()
-
-        self.activeVisit[visit] = Visit(visitId=visit, name=name)
-
+        self.activeVisit[visit] = Visit(visitId=visit, consumer=consumer, name=name)
         return self.activeVisit[visit]
 
-    def releaseVisit(self, visit):
+    def releaseVisit(self, visit, consumer=None):
+        """ Allocate a new visit. """
+        if visit == self.visit0.visitId:
+            return
+
         if self.activeVisit[visit] is None:
             raise VisitNotActiveError()
 
@@ -50,16 +75,17 @@ class VisitManager(object):
 
 
 class Visit(object):
-    def __init__(self, visitId, name=None):
+    def __init__(self, visitId, consumer=None, name=None):
         self.visitId = visitId
         self.name = name
+        self.consumer = consumer
         self.__agcFrameId = 0
         self.__fpsFrameId = 0
         self.iAmDead = False
         self.idLock = threading.RLock()
 
     def __str__(self):
-        return (f"Visit(name={self.name} visitId={self.visitId} "
+        return (f"Visit(name={self.name} consumer={self.consumer} visitId={self.visitId} "
                 f"fpsFrame={self.__fpsFrameId} agcFrame={self.__agcFrameId})")
 
     def stop(self):
@@ -90,3 +116,17 @@ class Visit(object):
             self.__fpsFrameId += 1
 
         return self.visitId * 100 + frameIdx
+
+
+class VisitO(Visit):
+    def __init__(self, visit, consumer='fps'):
+        Visit.__init__(self, visit, consumer=consumer)
+        self.consumers = dict(sps=True, agc=True)
+
+    def getVisit(self, consumer):
+        """ """
+        if self.consumers[consumer]:
+            self.consumers[consumer] = False
+            return True
+
+        return False
