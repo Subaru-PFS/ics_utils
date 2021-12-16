169b4c017c3accb69d179dbf21291b29efcef365
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-12-16 16:41:41 -0500

    unifying actors threading wrappers, that should be the only place

--- python/ics/utils/threading.py
+++ python/ics/utils/threading.py
@@ -4,14 +4,25 @@ from functools import partial
 from actorcore.QThread import QThread
 
 
+def getThread(instance):
+    """ Be robust about being called from QThread itself or the ActorCmd.py"""
+    if isinstance(instance, QThread):
+        return instance
+    elif hasattr(instance, 'controller') and isinstance(instance.controller, QThread):
+        return instance.controller
+    else:
+        raise RuntimeError('havent found any available thread to put func on')
+
+
 def putMsg(func):
     def wrapper(self, cmd, *args, **kwargs):
-        self.putMsg(partial(func, self, cmd, *args, **kwargs))
+        thread = getThread(self)
+        thread.putMsg(partial(func, self, cmd, *args, **kwargs))
 
     return wrapper
 
 
-def putMsg2(func):
+def putAndExit(func):
     def wrapper(self, cmd, *args, **kwargs):
         thr = QThread(self.actor, str(time.time()))
         thr.start()
@@ -21,8 +32,7 @@ def putMsg2(func):
     return wrapper
 
 
-def threaded(func):
-    @putMsg
+def mhsFunc(func):
     def wrapper(self, cmd, *args, **kwargs):
         try:
             return func(self, cmd, *args, **kwargs)
@@ -32,12 +42,64 @@ def threaded(func):
     return wrapper
 
 
+def threaded(func):
+    @putMsg
+    @mhsFunc
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 def singleShot(func):
-    @putMsg2
+    @putAndExit
+    @mhsFunc
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+class locking:
+    """ There is no safe way to know whether a QThread is actually doing something.
+        Its either waiting from queue.get(timeout=..) or calling a function. this class is basically a workaround. """
+
+    @staticmethod
+    def isLocked(thread):
+        try:
+            return thread.onGoingCmd
+        except AttributeError:
+            return False
+
+    @staticmethod
+    def lock(thread, cmd):
+        thread.onGoingCmd = cmd
+
+    @staticmethod
+    def unlock(thread):
+        thread.onGoingCmd = False
+
+
+def checkAndPut(func):
     def wrapper(self, cmd, *args, **kwargs):
+        thread = getThread(self)
+        if locking.isLocked(thread):
+            raise RuntimeWarning(f'{thread.name} is busy')
+
+        thread.putMsg(partial(func, self, cmd, *args, **kwargs))
+
+    return wrapper
+
+
+def blocking(func):
+    @checkAndPut
+    @mhsFunc
+    def wrapper(self, cmd, *args, **kwargs):
+        thread = getThread(self)
+        locking.lock(thread, cmd)
         try:
             return func(self, cmd, *args, **kwargs)
-        except Exception as e:
-            cmd.fail('text=%s' % self.actor.strTraceback(e))
+        finally:
+            locking.unlock(thread)
 
     return wrapper
