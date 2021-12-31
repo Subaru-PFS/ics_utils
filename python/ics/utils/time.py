b6b83885f045ebdcd6c21c54c2950562c62c9c79
Arnaud Le Fur <afur@princeton.astro.edu>
2021-12-31 16:51:45 -0500

    rephrase datetime class

--- python/ics/utils/time.py
+++ python/ics/utils/time.py
@@ -1,29 +1,36 @@
-from datetime import datetime, timedelta
+from datetime import datetime as pydt
 
 from ics.utils.sps.spectroIds import getSite
+from pytz import timezone
 
 
-def hstTime():
-    """Crude HST Time"""
-    return datetime.utcnow() - timedelta(hours=10)
+class PfsDatetime(pydt):
+    """ Regular python datetime with tzformat method to get local timezone isoformat."""
+
+    def setLocalTZ(self, timezone):
+        """ Set a local timezone for string conversion. """
+        self.localTZ = timezone
+
+    def tzformat(self, fmt='%Y-%m-%d %H:%M:%S %Z'):
+        """ Get datetime as isoformat + timezone, no microsecond by default."""
+        return self.astimezone(self.localTZ).strftime(fmt)
 
 
-class TimeGetter(object):
+class datetime(object):
     """ generate correct datetime according to site"""
-    def __init__(self):
-        site = getSite()
 
-        if site == 'S':
-            self.getTime = hstTime
-            self.timezone = 'HST'
-        else:
-            self.getTime = datetime.utcnow
-            self.timezone = 'UTC'
+    UTC = timezone('UTC')
+    HST = timezone('US/Hawaii')
+    site = getSite()
+    localTZ = HST if site == 'S' else UTC
 
-        self.site = site
-
-    def __call__(self, *args, **kwargs):
-        return self.getTime()
-
-    def format(self, timestamp):
-        return f'{timestamp.replace(microsecond=0).isoformat()}.{self.timezone}'
+    @classmethod
+    def now(cls):
+        datetimeObj = pydt.utcnow()
+        # force utc and localize this datetime.
+        pfsDatetime = PfsDatetime(year=datetimeObj.year, month=datetimeObj.month, day=datetimeObj.day,
+                                  hour=datetimeObj.hour, minute=datetimeObj.minute, microsecond=datetimeObj.microsecond,
+                                  tzinfo=datetime.UTC)
+        # set local timezone.
+        pfsDatetime.setLocalTZ(cls.localTZ)
+        return pfsDatetime
