4c539afa89fa7c9d73e1f19f82eb0b4051f6a89e
Arnaud Le Fur <afur@princeton.astro.edu>
2022-05-18 03:13:47 -1000

    instConfig accept optional idDict

--- python/ics/utils/instdata.py
+++ python/ics/utils/instdata.py
@@ -7,13 +7,12 @@ from ics.utils.actors import findProductAndInstance
 class InstConfig(dict):
     """Inst Config class to handle per-actor config yaml file."""
 
-    def __init__(self, actorName, logLevel=20):
+    def __init__(self, actorName, idDict=None, logLevel=20):
         super().__init__()
         self.logger = logging.getLogger('config')
         self.logger.setLevel(logLevel)
 
-        productName, instanceName = findProductAndInstance(actorName)
-        self.idDict = dict(actorName=actorName, productName=productName, instanceName=instanceName)
+        self.idDict = self.fetchIds(actorName, idDict=idDict)
         self.load()
 
     @property
@@ -28,6 +27,13 @@ class InstConfig(dict):
     def filepath(self):
         # retrieve config filepath, can be useful for book-keeping.
         return instdataIO.absFilepath('config', 'actors', self.productName)
+
+    def fetchIds(self, actorName, idDict=None):
+        """fetch ids from product and optional id dictionary."""
+        idDict = dict() if idDict is None else idDict
+        productName, instanceName = findProductAndInstance(actorName)
+        idDict.update(actorName=actorName, productName=productName, instanceName=instanceName)
+        return idDict
 
     def loadConfig(self):
         """Load per-actor yaml file."""
