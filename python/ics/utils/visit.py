897502901e7824ebb424935cbc4747d23bcbc2fb
Arnaud Le Fur <afur@princeton.astro.edu>
2022-06-13 10:46:37 -1000

    add context manager for visit, more flexibility to visit0

--- python/ics/utils/visit.py
+++ python/ics/utils/visit.py
@@ -1,4 +1,3 @@
-import logging
 import threading
 
 from ics.utils.opdb import opDB
@@ -29,7 +28,7 @@ class VisitManager(object):
         self.activeVisit = dict()
 
     def reloadField(self):
-        """ Reload persisted pfsField. """
+        """Reload persisted pfsField."""
         try:
             pfsDesignId, visit0 = self.actor.instData.loadKey('pfsField')
             pfsDesign = PfsDesign.read(int(pfsDesignId, 16), dirName=self.actor.actorConfig['pfsDesign']['rootDir'])
@@ -40,7 +39,7 @@ class VisitManager(object):
         return pfsField
 
     def declareNewField(self, pfsDesignId):
-        """ Declare new field, read pfsDesign and get visit0. """
+        """Declare new field, read pfsDesign and get visit0."""
         self.finishField()
 
         pfsDesign = PfsDesign.read(pfsDesignId, dirName=self.actor.actorConfig['pfsDesign']['rootDir'])
@@ -52,49 +51,33 @@ class VisitManager(object):
 
         return pfsDesign, self.activeField.visit0
 
-    def getField(self, consumer):
-        """ """
+    def getField(self):
+        """Get active field."""
         if self.activeField is None:
             raise RuntimeError('no pfsDesign has been declared current...')
 
-        pfsDesignId = self.activeField.getPfsDesignId()
-        visit = self.getVisit(consumer)
-
-        return pfsDesignId, visit
+        return self.activeField
 
     def finishField(self):
-        """ """
+        """Finish declaredCurrentPfsDesign."""
         self.activeField = None
         self.actor.instData.persistKey('pfsField', None, None)
 
-    def getVisit(self, consumer, name=None):
-        """ Get visit, visit0 if available otherwise new one"""
-        if self.activeField and self.activeField.visit0.available(consumer):
-            return self.activeField.visit0
+    def getCurrentDesignId(self):
+        """Get declaredCurrentPfsDesignId."""
+        return self.getField().getPfsDesignId()
 
-        return self.newVisit(consumer, name=name)
+    def getVisit(self, caller, name=None):
+        """Get visit, visit0 if available otherwise new one."""
+        if self.activeField and self.activeField.visitAvailableFor(caller):
+            return self.activeField.getVisit(caller)
 
-    def newVisit(self, consumer, name=None):
-        """ Generate new visit. """
+        return self.newVisit(caller, name=name)
+
+    def newVisit(self, caller, name=None):
+        """Generate new visit."""
         visitId = self._fetchVisitFromGen2()
-        self.activeVisit[visitId] = Visit(visitId=visitId, consumer=consumer, name=name)
-        return self.activeVisit[visitId]
-
-    def releaseVisit(self, visitId=None, consumer=None):
-        """ Release active visit. """
-        if visitId is None:
-            try:
-                [visitId] = list(self.activeVisit.keys())
-            except ValueError:
-                raise RuntimeError(f'dont know which visit to release : {",".join(map(str, self.activeVisit.keys()))}')
-
-        # we are still doing slightly weird things for now, so I'll play safe
-        if visitId not in self.activeVisit.keys():
-            logging.warning(f'visitId:{visitId} not in active visits, fine for now...')
-            return
-
-        self.activeVisit[visitId].stop()
-        self.activeVisit.pop(visitId, None)
+        return Visit.fromCaller(visitId, caller, name=name)
 
     def _fetchVisitFromGen2(self, pfsDesignId=None):
         """Actually get a new visit from Gen2.
@@ -104,90 +87,97 @@ class VisitManager(object):
 
 
 class Visit(object):
-    def __init__(self, visitId, consumer=None, name=None):
-        # sps can have concurrent visit, beside resourceManager is already doing the job
-        consumer = f'sps{visitId}' if consumer == 'sps' else consumer
+    isActive = False
+    isFrozen = False
+    exposureTable = ""
 
+    def __init__(self, visitId, caller='iic', name=None):
         self.visitId = visitId
+        self.caller = caller
         self.name = name
-        self.consumer = consumer
-        self.__agcFrameId = 0
-        self.__fpsFrameId = 0
+
+        self.isActive = False
         self.iAmDead = False
+        self.__frameId = 0
         self.idLock = threading.RLock()
 
     def __str__(self):
-        return (f"Visit(name={self.name} consumer={self.consumer} visitId={self.visitId} "
-                f"fpsFrame={self.__fpsFrameId} agcFrame={self.__agcFrameId})")
+        return f"Visit(name={self.name} caller={self.caller} visitId={self.visitId} subVisit={self.__frameId}"
+
+    def __enter__(self):
+        """Context manager on with statement."""
+        self.isActive = True
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Context manager exit."""
+        self.isActive = False
+
+    @property
+    def isPopulated(self):
+        return opDB.fetchone(f'select pfs_visit_id from {self.exposureTable} where pfs_visit_id={self.visitId}')
+
+    @property
+    def isAvailable(self):
+        """Assess if visit0 is available for a given caller, not sure its quite robust, but it would do OK for now."""
+        used = self.isActive or (self.isPopulated and not self.isFrozen)
+        return not used
+
+    @staticmethod
+    def fromCaller(visitId, caller, name):
+        if caller == 'sps':
+            return SpsVisit(visitId, name)
+        elif caller in ['mcs', 'fps']:
+            return FpsVisit(visitId, name)
+        elif caller in ['ag', 'agc']:
+            return AgVisit(visitId, name)
+        else:
+            raise ValueError(f'unknown caller:{caller}')
+
+    def nextFrameId(self):
+        """Get subvisit frameId."""
+        if self.iAmDead:
+            raise VisitAlreadyDone()
+
+        with self.idLock:
+            frameIdx = self.__frameId
+            if frameIdx >= 100:
+                raise VisitOverflowed()
+            self.__frameId += 1
+
+        return self.visitId * 100 + frameIdx
+
+    def frameId(self):
+        """Frame id accessor."""
+        return self.__frameId
 
     def stop(self):
         """Declare that we should not be used any more."""
         self.iAmDead = True
 
-    def agcFrameId(self):
-        """ AGC frame id accessor. """
-        return self.__agcFrameId
-
-    def fpsFrameId(self):
-        """ FPS frame id accessor. """
-        return self.__fpsFrameId
-
-    def frameForAGC(self):
-        if self.iAmDead:
-            raise VisitAlreadyDone()
-
-        with self.idLock:
-            frameIdx = self.__agcFrameId
-            if frameIdx >= 100:
-                raise VisitOverflowed()
-            self.__agcFrameId += 1
-
-        return self.visitId * 100 + frameIdx
-
-    def frameForFPS(self):
-        if self.iAmDead:
-            raise VisitAlreadyDone()
-
-        with self.idLock:
-            frameIdx = self.__fpsFrameId
-            if frameIdx >= 100:
-                raise VisitOverflowed()
-            self.__fpsFrameId += 1
-
-        return self.visitId * 100 + frameIdx
-
-
-class VisitO(Visit):
-    def __init__(self, visitId):
-        Visit.__init__(self, visitId, consumer='pfs', name='visit0')
-
-    def available(self, consumer):
-        """Assess if visit0 is available for a given consumer, not sure its quite robust, but it would do OK for now."""
-        if consumer == 'sps':
-            table = 'sps_visit'
-        elif consumer in ['fps', 'mcs']:
-            table = 'mcs_exposure'
-        elif consumer in ['ag', 'agc']:
-            table = 'agc_exposure'
-            # not sure it would actually work so returning True for now..
-            return True
-        else:
-            raise ValueError('do not know what to do here ...')
-
-        visitAvailable = not opDB.fetchone(f'select pfs_visit_id from {table} where pfs_visit_id={self.visitId}')
-        return visitAvailable
-
-    def releaseVisit(self, consumer):
-        """"""
-        pass
+    def setFrozen(self, doFreeze=True):
+        """Set visit frozen."""
+        self.isFrozen = doFreeze
 
 
 class PfsField(object):
     """Hold pfsDesign and visit0."""
 
-    def __init__(self, pfsDesign, visitId):
+    def __init__(self, pfsDesign, visit0):
         self.pfsDesign = pfsDesign
-        self.visit0 = VisitO(int(visitId))
+        self.visit0 = visit0
+
+        self.visit = dict(ag=AgVisit(visit0, name='visit0'),
+                          fps=FpsVisit(visit0, name='visit0'),
+                          sps=SpsVisit(visit0, name='visit0'))
+
+    def getVisit(self, caller):
+        """Get visit for caller."""
+        return self.visit[caller]
+
+    def visitAvailableFor(self, caller):
+        """Is visit available for that caller."""
+        return self.getVisit(caller).isAvailable
 
     def getPfsDesignId(self):
         """Return current pfsDesignId."""
@@ -203,3 +193,25 @@ class PfsField(object):
             position = None
 
         return position
+
+
+class AgVisit(Visit):
+    isFrozen = True
+    exposureTable = 'agc_exposure'
+
+    def __init__(self, visitId, name=None):
+        Visit.__init__(self, visitId, 'ag', name=name)
+
+
+class FpsVisit(Visit):
+    exposureTable = 'mcs_exposure'
+
+    def __init__(self, visitId, name=None):
+        Visit.__init__(self, visitId, 'fps', name=name)
+
+
+class SpsVisit(Visit):
+    exposureTable = 'sps_visit'
+
+    def __init__(self, visitId, name=None):
+        Visit.__init__(self, visitId, 'sps', name=name)
