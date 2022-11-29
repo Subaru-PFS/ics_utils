46dc1fb5d18fee281e6e22de46e8f4af03561ea5
Arnaud Le Fur <afur@princeton.astro.edu>
2022-11-29 11:54:58 -0500

    generate off,on timestamp for dcb lamp keys

--- python/ics/utils/sps/lamps/utils/lampState.py
+++ python/ics/utils/sps/lamps/utils/lampState.py
@@ -1,7 +1,6 @@
 __author__ = 'alefur'
 
-from datetime import datetime as dt
-from datetime import timedelta
+import ics.utils.time as pfsTime
 
 warmingTime = dict(neon=15, xenon=15, krypton=15, argon=15, halogen=15, hgar=60, hgcd=60)
 allLamps = list(warmingTime.keys())
@@ -11,19 +10,17 @@ class LampState(object):
     """ Handle lamp state and keywords. """
 
     def __init__(self):
-        start = dt.utcnow()
         self.state = 'unknown'
-        self.onTimestamp = start
-        self.offTimestamp = start
+        self.onTimestamp = self.offTimestamp = pfsTime.timestamp()
 
     @property
     def lampOn(self):
         return self.state == 'on'
 
     def __str__(self):
-        # return f'{self.state},{self.onTimestamp.isoformat()},{self.offTimestamp.isoformat()}'
-        secs = 0 if not self.lampOn else int(round((dt.utcnow() - self.onTimestamp).total_seconds()))
-        return f'{self.state},{secs}'
+        return ','.join([f'{self.state}',
+                         f'{pfsTime.Time.fromtimestamp(self.offTimestamp).isoformat()}',
+                         f'{pfsTime.Time.fromtimestamp(self.onTimestamp).isoformat()}'])
 
     def setState(self, state, genTimeStamp=False):
         """ Update current state and generate timestamp is requested. """
@@ -33,18 +30,20 @@ class LampState(object):
 
     def genTimeStamp(self):
         """ Generate timestamp as utc time. """
+        now = pfsTime.timestamp()
+
         if self.lampOn:
-            self.onTimestamp = dt.utcnow()
+            self.onTimestamp = now
         else:
-            self.offTimestamp = dt.utcnow()
+            self.offTimestamp = now
 
     def switchOffTiming(self, seconds):
         """ Predict when the lamp is supposed to turn off """
-        return self.onTimestamp + timedelta(seconds=seconds)
+        return self.onTimestamp + seconds
 
     def elapsed(self):
         """ Return number of seconds since the lamp is actually on. """
         if not self.lampOn:
             return 0
 
-        return (dt.utcnow() - self.onTimestamp).total_seconds()
+        return pfsTime.timestamp() - self.onTimestamp
